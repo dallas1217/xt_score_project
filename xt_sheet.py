c4eee8143b78f434ac0dfd0e226a05ffe3c2a6ce
dallas1217 <215861804@qq.com>
2017-06-30 18:36:16 +0800

    Update xt_sheet.py

--- xt_sheet.py
+++ xt_sheet.py
@@ -8,13 +8,29 @@ from openpyxl import load_workbook
 wb=load_workbook('xt_score_template.xlsx')
 ws=wb.get_sheet_by_name('Sheet1')
 
-position='FWR'
-for row in ws.iter_rows(min_row=4,max_row=6):
-    for cell in row:
-        new_cell=ws['%s%d' %(cell.column,(cell.row+30))]
-        new_cell.style=cell.style
-        new_cell.value=cell.value
-        #ws['B%d' %(min_row+30)].value='Origi'
+pos_list=['FWR']
+l=[]
+
+for i in range(4,28,3):
+    default_pos=ws['A%s' %i].value
+    for position in pos_list:
+        if position in default_pos:
+            for row in ws.iter_rows(min_row=i,max_row=(i+2)):
+                for cell in row:
+                    if isinstance(ws['%s%d' %(cell.column,(cell.row+30))].value,'NoneType'):
+                        print('OK')
+                    #    new_cell=ws['%s%d' %(cell.column,(cell.row+30))]
+                    #    new_cell.style=cell.style
+                    #    new_cell.value=cell.value
+                    else:
+                        print('Not OK')
+                    #    new_cell=ws['%s%d' %(cell.column,(cell.row+33))]
+                    #    new_cell.style=cell.style
+                    #    new_cell.value=cell.value
+
+        else:
+             pass
+
+
 
 wb.save('xt_score_test.xlsx')
-
