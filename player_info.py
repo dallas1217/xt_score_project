990f71925e17b28476155898c50e1c9e5fca36ab
dallas1217 <215861804@qq.com>
2017-07-03 20:24:12 +0800

    Update player_info.py

--- player_info.py
+++ player_info.py
@@ -1,47 +1,56 @@
 #!/usr/local/python3.6/bin/python3.6
 
-import json
+import json,xlsxwriter
 from functools import reduce
 
+### Initialize json stats and return name,position,stats list
 def stats_info(field,info):
-    names=[]
-    stats=[]
+    stats_list=[]                                      #Return list to calculate,item in list is dictionary type
     for l in info[field]['players']:
+        player_info={}
         if 'isFirstEleven' in l or 'subbedInExpandedMinute' in l:
-            names.append(l['name'])
-            stats.append(l['stats'])
-    full_stats=dict(zip(names,stats))
-    return full_stats
+            player_info['name']=l['name']              #Player name
+            player_info['position']=l['position']      #Player position
+            player_info['stats']=l['stats']            #Player stats in json file
+            stats_list.append(player_info)
+    return stats_list
 
-### Players statics about Non-GK players,return dictionary type
+
 def normal_stats(stats):
-    total_stat={}
-    stat={}
+    stats_list=[]        #Return list that input into excel,item in list is dictionary type
+    full_stats={}        #Full stats from json,items is in item_list
+    xt_stats={}          #XT score stats,items is in stat_list and gk_list
     item_list=['aerialSuccess', 'aerialsTotal', 'aerialsWon', 'claimsHigh', 'clearances', 'collected', 'cornersAccurate', 'cornersTotal', 'defensiveAerials', 'dispossessed', 'dribbleSuccess', 'dribbledPast', 'dribblesAttempted', 'dribblesLost', 'dribblesWon', 'errors', 'foulsCommited', 'interceptions', 'offensiveAerials', 'offsidesCaught', 'parriedSafe', 'passSuccess', 'passesAccurate', 'passesKey', 'passesTotal', 'possession', 'ratings', 'shotsBlocked', 'shotsOffTarget', 'shotsOnTarget', 'shotsTotal', 'tackleSuccess', 'tackleSuccessful', 'tackleUnsuccesful', 'tacklesTotal', 'throwInAccuracy', 'throwInsAccurate', 'throwInsTotal', 'totalSaves', 'touches']
-
-    stat_list=['aerialsWon','aerialsTotal','shotsOnTarget','shotsTotal','dribblesAttempted','dribblesWon','passesAccurate','passesTotal','clearances','interceptions','tackleSuccessful','passesKey','errors','dribbledPast']
-
+    stat_list=['aerialsWon','aerialsTotal','shotsOnTarget','shotsTotal','dribblesAttempted','dribblesWon','clearances','interceptions','tackleSuccessful','passesKey','errors','dribbledPast']
+    gk_list=['totalSaves','claimsHigh','collected','parriedSafe','errors']
     ### Calculate single item in item list
-    for item in item_list:
-        if item in stats:
-            total_stat[item]=single_item(stats[item])
+    for line in stats:
+        player_stats={}      #Total stats,includs name,position and XT score stats
+        player_stats['name']=line['name']
+        player_stats['position']=line['position']
+        for item in item_list:
+            if item in line['stats']:
+                full_stats[item]=single_item(line['stats'][item])
+            else:
+                full_stats[item]=0.0
+        if 'GK' in line['position']:
+            for item in item_list:
+                            for item in item_list:
+                if item in gk_list:
+                    xt_stats[item]=full_stats[item]
+            xt_stats['passSuccess']=reduce(percent_item,[full_stats['passesAccurate'],full_stats['passesTotal']])
         else:
-            total_stat[item]=0.0
+            for item in item_list:
+                if item in stat_list:
+                    xt_stats[item]=full_stats[item]
+            xt_stats['passSuccess']=reduce(percent_item,[full_stats['passesAccurate'],full_stats['passesTotal']])
+            xt_stats['aerialSuccess']=reduce(percent_item,[full_stats['aerialsWon'],full_stats['aerialsTotal']])
+            xt_stats['shotSuccess']=reduce(percent_item,[full_stats['shotsOnTarget'],full_stats['shotsTotal']])
+            xt_stats['dribbleSuccess']=reduce(percent_item,[full_stats['dribblesWon'],full_stats['dribblesAttempted']])
+        player_stats['stats']=xt_stats.copy()
+        stats_list.append(player_stats)
+    return stats_list
 
-    ### Add stat that need to show in worksheet to dictionary and return
-    for item in item_list:
-        if item in stat_list:
-            stat[item]=total_stat[item]
-
-    stat['passSuccess']=reduce(percent_item,[stat['passesAccurate'],stat['passesTotal']])
-    stat['aerialSuccess']=reduce(percent_item,[stat['aerialsWon'],stat['aerialsTotal']])
-    
-        stat['shotSuccess']=reduce(percent_item,[stat['shotsOnTarget'],stat['shotsTotal']])
-    stat['dribbleSuccess']=reduce(percent_item,[stat['dribblesWon'],stat['dribblesAttempted']])
-
-    return stat
-
-### Item calculate,including single calculate and percent calculate,return float type.Please noted that in percent calculate
 def single_item(item):
     count=0
     for i in item.values():
@@ -52,7 +61,7 @@ def percent_item(success,total):
     if total==0.0:
         return 0.0
     if success<=total:
-        return float('%.1f' %(success/total*100))
+        return round((success/total*100))
     else:
         print("Total item must bigger than success item!")
 
@@ -62,8 +71,4 @@ if __name__=='__main__':
         info=json.loads(line)
     f.close()
     away_stats=stats_info('away',info)
-    home_stats=stats_info('home',info)
-    for name,stat in home_stats.items():
-        print(name,normal_stats(stat))
-    for name,stat in away_stats.items():
-        print(name,normal_stats(stat))
+    print(normal_stats(away_stats))
